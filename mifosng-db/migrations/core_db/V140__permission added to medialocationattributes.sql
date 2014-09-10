d942d8abcefd9b02f7071cea61341c1c7b7a8912
kiran <kiran@hugotechnologies.com>
2014-09-10 12:56:18 +0530

    .

--- mifosng-db/migrations/core_db/V140__permission added to medialocationattributes.sql
+++ mifosng-db/migrations/core_db/V140__permission added to medialocationattributes.sql
@@ -1,8 +1,9 @@
 INSERT IGNORE INTO `m_permission`(grouping,code,entity_name,action_name,can_maker_checker) 
 VALUES ('BILLMASTER','CREATE_MEDIAASSETLOCATIONATTRIBUTES','MEDIAASSETLOCATIONATTRIBUTES','CREATE',1);
-INSERT IGNORE INTO `m_code`(id,code_name,is_system_defined,code_description) VALUES(52,'Event Category',0,'category for events');
-INSERT IGNORE INTO `m_code_value`(code_id,code_value,order_position) VALUES(52,'Live Event',0);
-INSERT IGNORE INTO `m_code_value`(code_id,code_value,order_position) VALUES(52,'VOD',0);
+INSERT IGNORE INTO `m_code`(id,code_name,is_system_defined,code_description) VALUES(null,'Event Category',0,'category for events');
+select @a_lid:=last_insert_id();
+INSERT IGNORE INTO `m_code_value`(code_id,code_value,order_position) VALUES(null,@a_lid,,'Live Event',0);
+INSERT IGNORE INTO `m_code_value`(code_id,code_value,order_position) VALUES(null,@a_lid,,'VOD',0);
 
 
 
